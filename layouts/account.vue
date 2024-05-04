18b1a23bf95d2d2248670eb598268da5a5e2d2ed
msavulescu <msavulescu@wallbid.io>
2024-05-04 20:14:45 +0300

    Modified layout.

--- layouts/account.vue
+++ layouts/account.vue
@@ -15,7 +15,7 @@
       <div class="w-full">
         <div class="flex flex-col items-center justify-center px-4 sm:px-0">
           <div
-            class="h-[1000px] flex rounded-2xl shadow-lg sm:w-3/4 lg:w-1/2 sm:mx-0 bg-black/15 text-center ring-2 ring-black/10 backdrop-blur-sm"
+            class="h-full flex rounded-2xl shadow-lg sm:w-3/4 lg:w-1/2 sm:mx-0 bg-black/15 text-center ring-2 ring-black/10 backdrop-blur-sm"
           >
             <slot />
           </div>
