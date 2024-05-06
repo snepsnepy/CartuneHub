d7dc75e38f3e60c991535f1834d47798bc880d13
snepsnepy <snepysneptest@gmail.com>
2024-05-06 12:29:37 +0300

    Modified layout.

--- layouts/account.vue
+++ layouts/account.vue
@@ -8,11 +8,11 @@
       class="hidden md:flex"
     />
   </div>
-  <div class="w-full flex justify-center my-auto">
+  <div class="w-full flex justify-center items-center my-auto h-full">
     <div class="w-full">
       <div class="flex flex-col items-center justify-center px-4 sm:px-0">
         <div
-          class="h-full flex rounded-2xl shadow-lg sm:w-3/4 lg:w-1/2 sm:mx-0 bg-black/15 text-center ring-2 ring-black/10 backdrop-blur-sm"
+          class="flex rounded-2xl shadow-lg sm:w-3/4 lg:w-1/2 sm:mx-0 bg-black/15 text-center ring-2 ring-black/10 backdrop-blur-sm"
         >
           <slot />
         </div>
