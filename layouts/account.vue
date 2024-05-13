d2a8fc51c568611ceeea3052b4cdc6b9ee35c0d0
snepsnepy <snepysneptest@gmail.com>
2024-05-13 15:47:17 +0300

    Modified account layout.

--- layouts/account.vue
+++ layouts/account.vue
@@ -1,13 +1,9 @@
 <template>
-  <div class="w-full flex justify-center items-center h-full py-20">
-    <div class="w-full">
-      <div class="flex flex-col items-center justify-center px-4 sm:px-0">
-        <div
-          class="flex rounded-2xl shadow-lg sm:w-3/4 lg:w-1/2 sm:mx-0 bg-white/15 text-center ring-2 ring-black/10 backdrop-blur-sm"
-        >
-          <slot />
-        </div>
-      </div>
+  <div class="flex justify-center items-center h-screen">
+    <div
+      class="flex flex-row rounded-2xl shadow-lg sm:w-3/4 lg:w-1/2 sm:mx-0 bg-white/15 text-center ring-2 ring-black/10 backdrop-blur-sm"
+    >
+      <slot />
     </div>
   </div>
 </template>
